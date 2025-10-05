3a1353a052ce42850afafa7f634ac4ff79cb8957
apor <soltesz.apor@ckik.hu>
2025-10-05 14:47:25 +0200

    Házi

--- házi feladat.py
+++ házi feladat.py
@@ -54,3 +54,19 @@ print(f"{fahrenheit} °F = {celsius:.2f} °C")
 celsius2 = random.randint(-30, 100)
 fahrenheit2 = (celsius2 * 9 / 5) + 32
 print(f"{celsius2} °C = {fahrenheit2:.2f} °F")
+# Bekérjük az időt másodpercben
+total_seconds = input("Írd be az időt másodpercben: ")
+
+try:
+    total_seconds = int(total_seconds)
+    if total_seconds < 0:
+        print("Kérlek adj meg egy pozitív számot!")
+    else:
+        hours = total_seconds // 3600
+        minutes = (total_seconds % 3600) // 60
+        seconds = total_seconds % 60
+
+        print(f"{hours} óra, {minutes} perc, {seconds} másodperc")
+except ValueError:
+    print("Kérlek számot adj meg!")
+
